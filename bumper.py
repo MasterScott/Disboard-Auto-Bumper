b8c4ddda328214786fbd01acc2d670a4b6a1b03b
Feldwor <BoQsc@users.noreply.github.com>
2021-05-26 22:58:38 +0300

    Update bumper.py

--- bumper.py
+++ bumper.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 # Uncomment the line below if you are wanting to host this on heroku and are using an environment variable to store the token.
 #token = os.getenv("TOKEN")
 # If you are using this on a server or your home pc uncomment the line below and put the discord token for the account you want it to auto bump on.
-#token = "TOKEN"
+token = os.environ['BOT_TOKEN']
 
 bot = commands.Bot(command_prefix = "--", self_bot=True)
 
