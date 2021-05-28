18b2b0f4bd73771d3b388b0ded4738b32b45c57a
Feldwor <BoQsc@users.noreply.github.com>
2021-05-28 13:23:20 +0300

    testing the  tokens

--- bumper.py
+++ bumper.py
@@ -1,27 +1,13 @@
-import discord, os, time
-from discord.ext import commands
+import os
+import discord
+from dotenv import load_dotenv
 
+load_dotenv()
+TOKEN = os.getenv('ODQ3Nzc5OTU5MzU1NDczOTIz.YLDCnQ.eh7bmOZKh2fYVEUnFq6omizHFHU')
+client = discord.Client()
 
-# Uncomment the line below if you are wanting to host this on heroku and are using an environment variable to store the token.
-#token = os.getenv("TOKEN")
-# If you are using this on a server or your home pc uncomment the line below and put the discord token for the account you want it to auto bump on.
-# token = os.environ['BOT_TOKEN']
-token = "ODQ3Nzc5OTU5MzU1NDczOTIz.YLDCnQ.eh7bmOZKh2fYVEUnFq6omizHFHU"
-bot = commands.Bot(command_prefix = "--", self_bot=True)
-
-@bot.event
+@client.event
 async def on_ready():
-    print("Auto Bumper Is Online!")
+    print(f'{client.user} has connected to Discord!')
 
-
-
-
-@bot.command()
-async def bla(ctx):
-    while True:
-        await ctx.send("!d bump")
-        time.sleep(8125)
-
-
-
-bot.run(token)
+client.run(TOKEN)
