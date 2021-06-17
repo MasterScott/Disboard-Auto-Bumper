6ff566aa5ccf7ebcd1c53855592e33247f881ee5
Feldwor <BoQsc@users.noreply.github.com>
2021-06-17 14:32:59 +0300

    Update bumper.py

--- bumper.py
+++ bumper.py
@@ -15,7 +15,6 @@ bot = commands.Bot(command_prefix = "--", self_bot=True)
 async def on_ready():
     print("Auto Bumper Is Online!")
     #print(await bot.fetch_user(token))
-    print(bot.owner_id)
     print(bot.user)
 
 @bot.command()
