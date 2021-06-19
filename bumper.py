529aa9a2932fb195034c908ad927878d61788e90
Feldwor <BoQsc@users.noreply.github.com>
2021-06-19 10:52:41 +0300

    Update bumper.py

--- bumper.py
+++ bumper.py
@@ -16,7 +16,8 @@ async def on_ready():
     print("Auto Bumper Is Online!") 
     print("Auto Bumper " + str(botReceive.user) + " | " + str(botReceive.user.id)) # https://discordpy.readthedocs.io/en/stable/ext/commands/api.html?highlight=run#discord.ext.commands.Bot.user
     print (str(await botReceive.fetch_channel("855039765711552515")))
-    await botReceive.fetch_channel("855039765711552515").send("found channel");
+    channnel = await botReceive.fetch_channel("855039765711552515");
+    await channnel.send("found channel")
     print (str(botReceive.get_channel("general")))
     #server = bot.get_server(member.server)
     #channel = discord.utils.get(server.text_channels, name='general')
